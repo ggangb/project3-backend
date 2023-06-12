61e403c9a8e14273203329a88621d795f4b6f186
ggangb <105210346+ggangb@users.noreply.github.com>
2023-06-12 23:09:29 +0900

    메인페이지 순위표작업중

--- src/main/java/com/example/project3/controller/HomeController.java
+++ src/main/java/com/example/project3/controller/HomeController.java
@@ -20,33 +20,40 @@ import org.springframework.web.util.UriComponentsBuilder;
 @RestController
 public class HomeController {
 
-	@CrossOrigin(origins = "http://localhost:8080")
 	@GetMapping("/news")
 	public String news() {
 		String query = "EPL";
 		ByteBuffer buffer = StandardCharsets.UTF_8.encode(query);
 		String encode = StandardCharsets.UTF_8.decode(buffer).toString();
-		
-		URI uri = UriComponentsBuilder
-				.fromUriString("https://openapi.naver.com")
-				.path("/v1/search/news.json")
-				.queryParam("query", encode)
-				.encode()
-				.build()
-				.toUri();
-		
+
+		URI uri = UriComponentsBuilder.fromUriString("https://openapi.naver.com").path("/v1/search/news.json")
+				.queryParam("query", encode).encode().build().toUri();
+
 		RestTemplate restTemplate = new RestTemplate();
-		
-		RequestEntity<Void> req = RequestEntity
-				.get(uri)
-				.header("X-Naver-Client-Id", "8WncpxE9lBzklQhOajm2")
-				.header("X-Naver-Client-Secret", "MuNmGX8Dwc")
-				.build();
-		ResponseEntity<String> result = restTemplate.exchange(req,  String.class);
-		String datas = result.getBody().replaceAll("&apos;", "'");
-		datas = datas.replaceAll("&quot;", "");
-		datas = datas.replaceAll("<(/)?([a-zA-Z]*)(\\\\s[a-zA-Z]*=[^>]*)?(\\\\s)*(/)?>", "");
-			
+
+		RequestEntity<Void> req = RequestEntity.get(uri).header("X-Naver-Client-Id", "8WncpxE9lBzklQhOajm2")
+				.header("X-Naver-Client-Secret", "MuNmGX8Dwc").build();
+		ResponseEntity<String> result = restTemplate.exchange(req, String.class);
+		String datas = result.getBody().replaceAll("&quot;", "")
+										.replaceAll("<(/)?([a-zA-Z]*)(\\\\s[a-zA-Z]*=[^>]*)?(\\\\s)*(/)?>", "")
+										.replaceAll("&apos;", "'");
 		return datas;
 	}
+
+	@GetMapping("/table")
+	public String table() {
+		String query = "PL";
+
+		URI uri = UriComponentsBuilder
+				.fromUriString("https://api.football-data.org/v4/competitions/" + query + "/standings").encode().build()
+				.toUri();
+		RestTemplate restTemplate = new RestTemplate();
+
+		RequestEntity<Void> req = RequestEntity.get(uri).header("X-Auth-token", "c3094ac91e0345eaa953aaf134884aa8")
+				.build();
+		ResponseEntity<String> result = restTemplate.exchange(req, String.class);
+
+		return result.getBody();
+	}
+
 }
