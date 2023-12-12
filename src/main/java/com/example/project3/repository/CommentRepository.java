d8553bc378e2ad9f1998393c2aba359455d4d851
ggangb <105210346+ggangb@users.noreply.github.com>
2023-12-12 16:18:28 +0900

    대댓글 작업완료

--- src/main/java/com/example/project3/repository/CommentRepository.java
+++ src/main/java/com/example/project3/repository/CommentRepository.java
@@ -14,13 +14,23 @@ import com.example.project3.models.Comment;
 
 public interface CommentRepository extends MongoRepository<Comment, String> {
 	
-		List<Comment>  findAllOrderByReforder(Sort sort);
 		
-		List<Comment> findByRefOrderByReforderDesc(Long ref);
+		//댓글그룹목록을 가져오는 메소드
+		List<Comment> findByRef(Long ref);
 		
+		//댓글목록 불러오는 메소드
 		Page<Comment> findAllByPostId(String postId, Pageable pageable);
 		
+		//자식댓글 추가로 refOrder가 바뀌어야할 댓글목록을 가져오는 메소드
+		@Query("{'ref': ?0, 'reforder': {$gt: ?1}}")
+		List<Comment> updateRefOrder(Long ref, int refOrder);
+		
+		//부모댓글 그룹의 level 최댓값인 항목 구하는 메소드
+		@Query(value = "{'ref': ?0}", sort = "{'level': -1}", fields = "{'level': 1}")
+		List<Comment> findMaxLevelByRef(Long ref);
+		
+		
+		//게시글 삭제시 댓글삭제
 		void deleteByPostId(String postId);
 
-	
 }
--- src/main/java/com/example/project3/service/CommentService.java
+++ src/main/java/com/example/project3/service/CommentService.java
@@ -26,6 +26,7 @@ public class CommentService {
 	@Autowired
 	SequenceGeneratorService sequenceGeneratorService;
 	
+	//부모댓글저장
 	public boolean commentSave(Comment comment) {
 		comment.setRef(sequenceGeneratorService.generateSequence(Comment.SEQUENCE_NAME));
 		comment.setChildnum( 0);
@@ -41,6 +42,7 @@ public class CommentService {
 		}
 	}
 	
+	//댓글목록
 	public Page<Comment> getCommentList(String postId, Pageable pageable) {
 		
 		Page<Comment> commentAll = commentRepository.findAllByPostId(postId, pageable);
@@ -48,85 +50,42 @@ public class CommentService {
 		return commentAll;
 	}
 	
+	//자식댓글저장
 	public void reCommentSave(Comment comment) {
 		
-		System.out.println("입력 대댓글" + comment);
+		//부모 댓글 데이터 가져오기
 		Optional<Comment> c = commentRepository.findById(comment.getParentnum());
 		Comment parent = c.get();
-		System.out.println("부모댓글" + parent);
 		
 		Comment child = comment;
 		
+		//자식댓글 추가에 따른 다른 댓글들의 refOrder값 수정후 자식댓글 refOrder값 가져오는 메소드 
+		int refOrderResult = refOrderUpdate(parent);
+		
+		//리턴이 0 이면 아무것도 하지않음
+		if(refOrderResult == 0) {
+			return;
+		}
+		//자식 댓글 저장
+		child.setRef(parent.getRef());
+		child.setLevel(parent.getLevel()+1);
+		child.setReforder(refOrderResult);
+		commentRepository.save(child);
+		
+		
+		//부모댓글의 자식수 업데이트
+		parent.setChildnum(parent.getChildnum()+1);
+		commentRepository.save(parent);
 			
-
-			List<Comment> cm = commentRepository.findByRefOrderByReforderDesc(parent.getRef());
-			Comment c1 = cm.get(0);
-			if(parent.getReforder() == 0) {
-				child.setRef(parent.getRef());
-				child.setReforder(c1.getReforder()+1);
-				child.setLevel(parent.getLevel()+1);
-				parent.setChildnum(parent.getChildnum()+1);
-				commentRepository.save(parent);
-				commentRepository.save(child);
-				commentRepository.save(c1);
-			} else {
-				child.setRef(parent.getRef());
-				child.setReforder(parent.getReforder()+1);
-				c1.setReforder(c1.getReforder()+1);
-				child.setLevel(parent.getLevel()+1);
-				parent.setChildnum(parent.getChildnum()+1);
-				commentRepository.save(parent);
-				commentRepository.save(child);
-				commentRepository.save(c1);
-			}
-		
-		
-		
-		
-//		
-//			List<Comment> cm = commentRepository.findByRefOrderByReforder(c1.getRef());
-//			Comment com = cm.get(0);
-//			System.out.println(com);
-//			comment.setReforder(com.getReforder());
-//			com.setReforder(com.getReforder()+1);
-//			c1.setChildnum(c1.getChildnum()+1);
-//			comment.setRef(c1.getRef());
-//			comment.setLevel(c1.getLevel()+1);
-//			System.out.println("댓글등록전 최종" + comment);
-//			commentRepository.save(c1);
-//			commentRepository.save(com);
-//			commentRepository.save(comment);
-
-		
-	
-//		if(c1.getChildnum() == 0) {
-//			comment.setReforder(1);
-//			c1.setChildnum((c1.getChildnum()+1));
-//			comment.setRef(c1.getRef());
-//			comment.setLevel((c1.getLevel()+1));
-//			commentRepository.save(c1);
-//			commentRepository.save(comment);
-//		} else {
-//			List<Comment> cm = commentRepository.findAll(Sort.by(Direction.DESC, "reforder"));
-//			Comment com1 = cm.get(0);
-//			System.out.println(com1);
-//			c1.setChildnum(c1.getChildnum()+1);
-//			comment.setRef(c1.getRef());
-//			comment.setReforder(com1.getReforder());
-//			com1.setReforder(com1.getReforder()+1);
-//			comment.setLevel((c1.getLevel()+1));
-//			commentRepository.save(com1);
-//			commentRepository.save(c1);
-//			commentRepository.save(comment);
-//		}
-		
 		
 	}
 	
+	//댓글 수정
 	public void recommentUpdate(Comment comment) {
 		commentRepository.save(comment);
 	}
 	
+	//댓글 삭제
 	public void recommentDelete(String contentId) {
 		Optional<Comment> c = commentRepository.findById(contentId);
 		Comment c1 = c.get();
@@ -135,5 +94,50 @@ public class CommentService {
 		commentRepository.save(c1);
 	}
 	
+	//자식댓글 refOrder구하는 메소드
+	private int refOrderUpdate(Comment comment) {
+		int saveLevel = comment.getLevel() + 1;
+		int refOrder = comment.getReforder();
+		int answerNum = comment.getChildnum();
+		Long ref = comment.getRef();
+		int answerSum = 0;
+		
+		//부모댓글 그룹 가져오기
+		List<Comment> refList = commentRepository.findByRef(ref);
+		
+		//부모댓글의 자식수의 합 구하기
+		for(int i=0; i<refList.size(); i++) {
+			answerSum += refList.get(i).getChildnum();
+		}
+		Comment commentmaxLevel = commentRepository.findMaxLevelByRef(ref).get(0);
+		int maxLevel = commentmaxLevel.getLevel();
+		
+		//저장할 자식댓글의 step과 부모댓글 그룹내의 최댓값 level의 비교
+		if(saveLevel < maxLevel) {
+			return answerSum + 1;
+		} else if (saveLevel == maxLevel) {
+				List<Comment> c = commentRepository.updateRefOrder(ref, refOrder + answerNum);
+				if(c != null) {	
+					for(int i=0; i<c.size(); i++) {
+						c.get(i).setReforder(c.get(i).getReforder()+1);
+						commentRepository.save(c.get(i));
+					}
+					return refOrder + answerNum + 1;
+				}
+				return refOrder + answerNum + 1;
+			
+		} else if (saveLevel > maxLevel) {
+			List<Comment> c = commentRepository.updateRefOrder(ref, refOrder + answerNum);
+				if(c != null) {					
+					for(int i=0; i<c.size(); i++) {
+						c.get(i).setReforder(c.get(i).getReforder()+1);
+						commentRepository.save(c.get(i));
+					}
+					return refOrder + 1;
+				}
+				return refOrder + 1;			
+		}	
+		return 0;
+	}
 	
 }
